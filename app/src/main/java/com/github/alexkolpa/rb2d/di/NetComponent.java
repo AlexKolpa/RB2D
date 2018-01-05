8f42604bdb9c0acad0c6119cfa183f2404c31019
Alex Kolpa <akolpa1990@gmail.com>
2018-01-05 08:48:50 +0100

    Setup dagger and retrofit

---
+++ app/src/main/java/com/github/alexkolpa/rb2d/di/NetComponent.java
@@ -0,0 +1,12 @@
+package com.github.alexkolpa.rb2d.di;
+
+import javax.inject.Singleton;
+
+import com.github.alexkolpa.rb2d.overview.OverviewActivity;
+import dagger.Component;
+
+@Singleton
+@Component(modules = { AppModule.class, NetModule.class })
+public interface NetComponent {
+	void inject(OverviewActivity activity);
+}
